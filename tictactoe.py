a2adcbd2b527d791453f5ababd1aa4741da94125
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-06-10 14:10:42 +1200

    process into matrix

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,7 @@
 # write your code here
-print("X O X")
-print("O X O")
-print("X X O")
+matrix = input("Enter cell: ")
+print("---------")
+print(f"| {matrix[0]} {matrix[1]} {matrix[2]} |")
+print(f"| {matrix[3]} {matrix[4]} {matrix[5]} |")
+print(f"| {matrix[6]} {matrix[7]} {matrix[8]} |")
+print("---------")
